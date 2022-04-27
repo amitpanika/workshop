93e78eebf31be57e3514a7a035a1bb51419f471d
amitpanika <97650486+amitpanika@users.noreply.github.com>
2022-04-27 18:33:27 +0530

    Add files via upload

---
+++ chocolate.java
@@ -0,0 +1,18 @@
+package chocolate;
+
+import java.util.ArrayList;
+
+public class chocolate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<String>BrandName = new ArrayList<String>();
+		BrandName.add("dairymilk");
+		BrandName.add("cadbury");
+		BrandName.add("kitkat");
+		System.out.println(BrandName);
+		
+	}
+
+}
